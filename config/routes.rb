2caa5915beae3feedfbc9d6f2573a96dd22dd402
Andreas Lubbe <andreaslubbe@gmail.com>
2013-09-07 01:51:41 +0200

    added is24 scraper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Trobox::Application.routes.draw do
+  
+  get 'expose/:id' => 'test#expose'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
