250ef622ba6b1fc73713497051d30dc2a639acf8
Andreas Lubbe <andreaslubbe@gmail.com>
2013-09-06 22:30:06 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trobox::Application.config.secret_key_base = 'd1fe885418d406da0153c621b08f4d62d4c683992c05d7488eac5165d31e3cdc26a5b4eeda20b00ff090b5f47b36f381b27ad37e57f42311a0196351ac8dae1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trobox::Application.config.session_store :cookie_store, key: '_trobox_session'
